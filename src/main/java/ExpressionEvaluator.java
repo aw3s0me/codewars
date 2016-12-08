d3aba1abe5b0e6773998b74996bf05bcadde11c8
aw3s0me <akorovin00@gmail.com>
2016-12-08 12:58:29 +0100

    started implementing evaluator + found links

--- src/main/java/ExpressionEvaluator.java
+++ src/main/java/ExpressionEvaluator.java
@@ -3,9 +3,14 @@ import java.util.Deque;
 /**
  * Created by korovin on 12/8/2016.
  * Evaluates mathematical expressions
+ * Implements the idea of https://en.wikipedia.org/wiki/Recursive_descent_parser
+ * Idea taken from: http://www.codecodex.com/wiki/Recursive_descent_parsing#Parser.java
  */
 public final class ExpressionEvaluator {
     public static Double evaluateExpression(Deque<String> input) {
+        for (String element: input) {
+
+        }
         return 0.0;
     }
 }
